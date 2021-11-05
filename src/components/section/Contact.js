380d83ac4f8fc2d375cac14204e0dd353a32ae6f
GKkrish123 <67182212+GKkrish123@users.noreply.github.com>
2021-11-05 16:34:57 +0530

    Update Contact.js

--- src/components/section/Contact.js
+++ src/components/section/Contact.js
@@ -1,7 +1,40 @@
-import React from 'react'
-import { contact, section5Title, social } from '../../profile'
+import React, {useState} from 'react'
+import { contact, section5Title, social, formSheet } from '../../profile'
 
 const Contact = () => {
+    const [formData, setformData] = useState({
+        name: "",
+        email: "",
+        subject: "",
+        message: ""
+    });
+
+    const {name, email, subject, message} = formData;
+
+    const handleChange=(e)=>{
+        setformData({...formData, [e.target.name]: e.target.value})
+    };
+
+    const submitForm=async(e)=>{
+        e.preventDefault();
+        try {
+            const response = await fetch(
+                formSheet, {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify([[name, email, subject, message, new Date().toLocaleString()]])
+                }
+            )
+            await response.json()
+            alert("Hey "+name+ ", your form got submitted. Thanks for reaching out.")
+            setformData({...formData, name: "",email: "",subject: "",message: ""})
+        } catch (error) {
+            console.log(error);
+            alert("Hey "+name+", there is an error in submitting the form. Please reach out through other media. Thanks")
+        }
+    };
     
     return (
         <div className="parallax">
@@ -14,19 +47,23 @@ const Contact = () => {
                 <div className="container">
             <div className="git-cont row">
                 <div className="col-12 col-sm-6 half">
-                    <form action={contact.contactUrl ? contact.contactUrl : "https://formspree.io"} method={contact.contactUrl ? "POST" : "GET"}>
-                        <input type="text" id="fname" name="firstname" placeholder="Your name" required></input>
-                        <input type="mail" id="mailid" name="Email" placeholder="Email Address" required></input>
-                        <input type="text" id="sub" name="Subject" placeholder="Subject" required></input>
-                        <textarea id="msg" name="message" placeholder="Message" required></textarea>
+                    <form onSubmit={submitForm}>
+                        <input type="text" id="fname" name="name" placeholder="Your name" value={name} required onChange={handleChange}></input>
+                        <input type="mail" id="mailid" name="email" placeholder="Email Address" value={email} required onChange={handleChange}></input>
+                        <input type="text" id="sub" name="subject" placeholder="Subject" value={subject} required onChange={handleChange}></input>
+                        <textarea id="msg" name="message" placeholder="Message" value={message} required onChange={handleChange}></textarea>
                         <button style={{cursor: 'pointer'}} type="submit">Send Message</button>
                     </form>
                 </div>
                 <div className="col-12 col-sm-6 half">
+                <div className="d-flex justify-content-center align-items-center flex-column">
+                    <p className="lead">
+                        If you are a recruiter, you may need my resume ;)       
+                    </p>
+                    {social.resume && <a title="Download Resume" href={social.resume} download><i className="fas fa-download"></i></a>}
                     <p className="lead">
                         {contact.pitch}        
                     </p>
-                <div className="d-flex justify-content-center align-items-center flex-column">
                 <div className="inline-block">
                     {social.linkedin && <a title="Visit Linkedin profile" rel="noopener noreferrer" target="_blank"  href={social.linkedin}><i className="fab fa-linkedin"></i></a>}
                     {social.facebook && <a title="Visit Facebok profile" rel="noopener noreferrer" target="_blank" href={social.facebook}><i className="fab fa-facebook"></i></a>}
@@ -34,7 +71,6 @@ const Contact = () => {
                     {social.instagram && <a title="Visit Instagram profile" rel="noopener noreferrer" target="_blank" href={social.instagram}><i className="fab fa-instagram"></i></a>}
                     {social.github && <a title="Visit Github profile" rel="noopener noreferrer" target="_blank" href={social.github}><i className="fab fa-github"></i></a>}<br/>
                 </div>
-                    {social.resume && <a title="Download Resume" href={social.resume} download><i className="fas fa-download"></i></a>}
                 </div>
                 </div>
             </div>
