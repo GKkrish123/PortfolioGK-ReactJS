34671ae6abcf74a7a7698b4227c1cd12b60299a5
GKkrish123 <67182212+GKkrish123@users.noreply.github.com>
2022-11-26 18:47:48 +0530

    Kawaii

---
+++ src/components/layouts/Kawaii.js
@@ -0,0 +1,117 @@
+import { memo, useEffect, useRef } from "react";
+import { Cat } from "react-kawaii";
+import { styler, tween, merge, action, easing } from "popmotion";
+
+const Kawaii = () => {
+  const catRef = useRef();
+  useEffect(() => {
+    if (catRef.current) {
+      const mouth = styler(catRef.current.querySelector("#Combined-Shape"));
+      const tongue = styler(
+        catRef.current.querySelector("#kawaii-face__tongue")
+      );
+      const eyeLeft = styler(
+        catRef.current.querySelector("#kawaii-face__eyes__circle:first-child")
+      );
+      const eyeRight = styler(
+        catRef.current.querySelector("#kawaii-face__eyes__circle:last-child")
+      );
+
+      const tail = styler(catRef.current.querySelector("#kawaii-cat_tail"));
+
+      tween({
+        from: { x: 29.23, y: 153.846, rotate: 0 },
+        to: { x: 29.23, y: 153.846, rotate: 20 },
+        ease: easing.backInOut,
+        duration: 1300,
+        yoyo: Infinity,
+      }).start(tail.set);
+
+      const showEye = tween({
+        from: { scaleY: 0.3 },
+        to: { scaleY: 1 },
+        duration: 400,
+      });
+
+      const show = tween({
+        from: { scaleY: 0.3 },
+        to: { scaleY: 1 },
+        duration: 200,
+      });
+
+      const blinkEye = tween({
+        from: { scaleY: 1 },
+        to: { scaleY: 0.3 },
+        duration: 100,
+        flip: 1,
+      });
+
+      const closeMouth = tween({
+        from: { scaleY: 1 },
+        to: { scaleY: 0.3 },
+        duration: 300,
+        flip: 1,
+      });
+
+      const tongueDisappear = tween({
+        from: { scaleY: 1 },
+        to: { scaleY: 0 },
+        duration: 300,
+        flip: 1,
+      });
+
+      const eyeLeftAction = action(({ complete }) => {
+        showEye.start({
+          update: eyeLeft.set,
+          complete: () => {
+            complete();
+            setInterval(() => blinkEye.start({ update: eyeLeft.set }), 2000);
+          },
+        });
+      });
+
+      const eyeRightAction = action(({ complete }) => {
+        showEye.start({
+          update: eyeRight.set,
+          complete: () => {
+            complete();
+            setInterval(() => blinkEye.start({ update: eyeRight.set }), 2000);
+          },
+        });
+      });
+
+      const mouthAction = action(({ complete }) => {
+        show.start({
+          update: mouth.set,
+          complete: () => {
+            complete();
+            setInterval(() => closeMouth.start({ update: mouth.set }), 3500);
+          },
+        });
+      });
+
+      const tongueAction = action(({ complete }) => {
+        show.start({
+          update: tongue.set,
+          complete: () => {
+            complete();
+            setInterval(
+              () => tongueDisappear.start({ update: tongue.set }),
+              3500
+            );
+          },
+        });
+      });
+
+      merge(eyeLeftAction, eyeRightAction, mouthAction, tongueAction).start();
+    }
+  }, []);
+
+  return (
+    <div ref={catRef} className="kawaii">
+      <Cat size={150} mood="excited" color="#596881" />
+    </div>
+  );
+};
+
+export default memo(Kawaii);
--- src/components/section/Contact.js
+++ src/components/section/Contact.js
@@ -1,118 +1,187 @@
-import React, {useState} from 'react'
-import { contact, section5Title, social } from '../../profile'
-import emailjs from '@emailjs/browser';
+import React, { useState } from "react";
+import { contact, section5Title, social } from "../../profile";
+import emailjs from "@emailjs/browser";
+import Kawaii from "../layouts/Kawaii";
 
 const Contact = () => {
+  const [invalidEmail, setInvalidEmail] = useState(false);
 
-    const [ invalidEmail, setInvalidEmail ] = useState(false);
-    
-    const [formData, setformData] = useState({
-        name: "",
-        email: "",
-        message: ""
-    });
-    
-    const {name, email, message} = formData;
-    
-    const sendEmail = (e) => {
-        e.preventDefault(); // Prevents default refresh by the browser
-        emailjs
-          .send("service_76ddsb7", "template_3ack8ia", formData, "rQiJxT-yYhtQm3NY4")
-          .then(
-            (result) => {
-              console.log("Message Sent -> ", result);
-              alert("Message Sent, Thanks for your attention. I have something for you ;).");
-              window.emojisplosion({
-                emojiCount: 25,
-                position() {
-                    const element = document.getElementById("Contact");
-                    const offset = element.getBoundingClientRect();
-                
-                    return {
-                      x: offset.left + element.clientWidth / 2,
-                      y: offset.top + element.clientHeight / 2,
-                    };
-                  },
-              });
-              setformData({
-                name: "",
-                email: "",
-                message: ""
-            })
+  const [formData, setformData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const { name, email, message } = formData;
+
+  const sendEmail = (e) => {
+    e.preventDefault(); // Prevents default refresh by the browser
+    emailjs
+      .send(
+        "service_76ddsb7",
+        "template_3ack8ia",
+        formData,
+        "rQiJxT-yYhtQm3NY4"
+      )
+      .then(
+        (result) => {
+          console.log("Message Sent -> ", result);
+          alert(
+            "Message Sent, Thanks for your attention. I have something for you ;)."
+          );
+          window.emojisplosion({
+            emojiCount: 25,
+            position() {
+              const element = document.getElementById("Contact");
+              const offset = element.getBoundingClientRect();
+
+              return {
+                x: offset.left + element.clientWidth / 2,
+                y: offset.top + element.clientHeight / 2,
+              };
             },
-            (error) => {
-              console.log("Error in sending message -> ", error);
-              alert("An error occurred, Please try to send message again");
-            }
-        );
-    }
-
-    const handleChange=(e)=>{
-        if(e.target.name === "email"){
-            setInvalidEmail(!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(e.target.value))
+          });
+          setformData({
+            name: "",
+            email: "",
+            message: "",
+          });
+        },
+        (error) => {
+          console.log("Error in sending message -> ", error);
+          alert("An error occurred, Please try to send message again");
         }
-        setformData({...formData, [e.target.name]: e.target.value})
-    };
+      );
+  };
 
-    // const submitForm=async(e)=>{
-    //     e.preventDefault();
-    //     try {
-    //         const response = await fetch(
-    //             formSheet, {
-    //                 method: "POST",
-    //                 headers: {
-    //                     "Content-Type": "application/json"
-    //                 },
-    //                 body: JSON.stringify([[name, email, message, new Date().toLocaleString()]])
-    //             }
-    //         )
-    //         await response.json()
-    //         alert("Hey "+name+ ", your form got submitted. Thanks for reaching out.")
-    //         setformData({...formData, name: "",email: "",message: ""})
-    //     } catch (error) {
-    //         console.log(error);
-    //         alert("Hey "+name+", there is an error in submitting the form. Please reach out through other media. Thanks")
-    //     }
-    // };
-    
-    return (
-        <div id="ContactMe" className="parallax">
-                <div data-aos="zoom-in-up" data-aos-once="true" className="git-form">
-                    <>
-                <div className="git-head-div text-center mx-auto">
-                        <h1 id="Contact" className="git-head">{section5Title}</h1>
-                </div>
-                </>
-                <div className="container">
-            <div className="git-cont row">
-                <div className="col-12 col-sm-6 half">
-                    <form onSubmit={sendEmail}>
-                        <input type="text" id="fname" name="name" placeholder="Your name" value={name} required onChange={handleChange}></input>
-                        <input type="mail" id="mailid" name="email" placeholder="Email Address" value={email} required onChange={handleChange}></input>
-                        {invalidEmail && <label className='error-text'>! Please enter a valid email address !</label>}
-                        <textarea id="msg" name="message" placeholder="Message" value={message} required onChange={handleChange}></textarea>
-                        <button style={{cursor: 'pointer'}} type="submit">Send Message</button>
-                    </form>
-                </div>
-                <div className="col-12 col-sm-6 half">
-                    <p className="lead">
-                        {contact.pitch}        
-                    </p>
-                <div className="d-flex justify-content-center align-items-center flex-column">
+  const handleChange = (e) => {
+    if (e.target.name === "email") {
+      setInvalidEmail(
+        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(e.target.value)
+      );
+    }
+    setformData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  // const submitForm=async(e)=>{
+  //     e.preventDefault();
+  //     try {
+  //         const response = await fetch(
+  //             formSheet, {
+  //                 method: "POST",
+  //                 headers: {
+  //                     "Content-Type": "application/json"
+  //                 },
+  //                 body: JSON.stringify([[name, email, message, new Date().toLocaleString()]])
+  //             }
+  //         )
+  //         await response.json()
+  //         alert("Hey "+name+ ", your form got submitted. Thanks for reaching out.")
+  //         setformData({...formData, name: "",email: "",message: ""})
+  //     } catch (error) {
+  //         console.log(error);
+  //         alert("Hey "+name+", there is an error in submitting the form. Please reach out through other media. Thanks")
+  //     }
+  // };
+
+  return (
+    <div id="ContactMe" className="parallax">
+      <div data-aos="zoom-in-up" data-aos-once="true" className="git-form">
+        <div className="contact-header">
+          <Kawaii />
+          <div className="git-head-div text-center mx-auto">
+            <h1 id="Contact" className="git-head">
+              {section5Title}
+            </h1>
+          </div>
+        </div>
+        <div className="container">
+          <div className="git-cont row">
+            <div className="col-12 col-sm-6 half">
+              <form onSubmit={sendEmail}>
+                <input
+                  type="text"
+                  id="fname"
+                  name="name"
+                  placeholder="Your name"
+                  value={name}
+                  required
+                  onChange={handleChange}
+                ></input>
+                <input
+                  type="mail"
+                  id="mailid"
+                  name="email"
+                  placeholder="Email Address"
+                  value={email}
+                  required
+                  onChange={handleChange}
+                ></input>
+                {invalidEmail && (
+                  <label className="error-text">
+                    ! Please enter a valid email address !
+                  </label>
+                )}
+                <textarea
+                  id="msg"
+                  name="message"
+                  placeholder="Message"
+                  value={message}
+                  required
+                  onChange={handleChange}
+                ></textarea>
+                <button style={{ cursor: "pointer" }} type="submit">
+                  Send Message
+                </button>
+              </form>
+            </div>
+            <div className="col-12 col-sm-6 half">
+              <p className="lead">{contact.pitch}</p>
+              <div className="d-flex justify-content-center align-items-center flex-column">
                 <div className="inline-block">
-                    {social.linkedin && <a title="Visit Linkedin profile" rel="noopener noreferrer" target="_blank"  href={social.linkedin}><i className="fab fa-linkedin"></i></a>}
-                    {social.instagram && <a title="Visit Instagram profile" rel="noopener noreferrer" target="_blank" href={social.instagram}><i className="fab fa-instagram"></i></a>}
-                    {social.github && <a title="Visit Github profile" rel="noopener noreferrer" target="_blank" href={social.github}><i className="fab fa-github"></i></a>}<br/>
+                  {social.linkedin && (
+                    <a
+                      title="Visit Linkedin profile"
+                      rel="noopener noreferrer"
+                      target="_blank"
+                      href={social.linkedin}
+                    >
+                      <i className="fab fa-linkedin"></i>
+                    </a>
+                  )}
+                  {social.instagram && (
+                    <a
+                      title="Visit Instagram profile"
+                      rel="noopener noreferrer"
+                      target="_blank"
+                      href={social.instagram}
+                    >
+                      <i className="fab fa-instagram"></i>
+                    </a>
+                  )}
+                  {social.github && (
+                    <a
+                      title="Visit Github profile"
+                      rel="noopener noreferrer"
+                      target="_blank"
+                      href={social.github}
+                    >
+                      <i className="fab fa-github"></i>
+                    </a>
+                  )}
+                  <br />
                 </div>
-                </div>
-                </div>
+              </div>
             </div>
-            </div>
+          </div>
         </div>
-            <p id="not-dark" className="Copy">2021 © Copyright <strong>{contact.copyright}</strong>. All Rights Reserved</p>
-        </div>
-    )
-    
-}
+      </div>
+      <p id="not-dark" className="Copy">
+        2021 © Copyright <strong>{contact.copyright}</strong>. All Rights
+        Reserved
+      </p>
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
